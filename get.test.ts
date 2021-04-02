0db7997205b1752b6d2129c3be2518ccaaffd8dd
Ryan Poe <baublet@gmail.com>
2021-04-02 08:11:41 -0500

    basic tests

--- get.test.ts
+++ get.test.ts
@@ -1,3 +1,69 @@
-it("works", () => {
-  expect(1).toEqual(1);
+import { get } from "./get";
+
+describe("basics", () => {
+  it("accesses basic properties", () => {
+    expect(
+      get({
+        object: {
+          test: 123,
+        },
+        path: "test",
+      })
+    ).toEqual(123);
+  });
+
+  it("accesses nested properties", () => {
+    expect(
+      get({
+        object: {
+          test: {
+            floofer: "nooter",
+          },
+        },
+        path: "test",
+      })
+    ).toEqual({
+      floofer: "nooter",
+    });
+
+    expect(
+      get({
+        object: {
+          test: {
+            floofer: "nooter",
+          },
+        },
+        path: "test.floofer",
+      })
+    ).toEqual("nooter");
+  });
+
+  it("accesses array properties", () => {
+    expect(
+      get({
+        object: {
+          test: {
+            floofer: ["nooter"],
+          },
+        },
+        path: "test.floofer",
+      })
+    ).toEqual(["nooter"]);
+  });
+});
+
+describe("slots", () => {
+  it("accesses properties by slots", () => {
+    expect(
+      get({
+        object: {
+          test: {
+            floofer: ["nooter", "scooter"],
+          },
+        },
+        path: "test.floofer.[]",
+        slots: [1],
+      })
+    ).toEqual("scooter");
+  });
 });
